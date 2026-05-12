31552bbd74d96ce395f536fb989c789629c4d458
victormori <victordaab@gmail.com>
2026-05-12 20:03:40 -0300

    Sistema de Cadastro Simples

--- Program.cs
+++ Program.cs
@@ -8,56 +8,86 @@ public class Program
     public static void Main()
     {
 
-        string nomeUsuario = "Victor";
-        string senhaUsuario = "victor@123";
+        Console.WriteLine("----------------CADASTRO----------------");
 
-        Console.WriteLine("---------LOGIN---------");
+        string senha = "";
+        int tamanho = 5;
 
-        Console.Write("Digite seu nome:");
+        Console.Write("Digite seu nome: ");
         string nome = Console.ReadLine();
 
-        Console.Write("Digite sua senha:");
-        string senha = Console.ReadLine();
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("-----------------------");
+        Console.Write("Digite seu email: ");
+        string email = Console.ReadLine();
 
-        if (nome == nomeUsuario && senha == senhaUsuario)
+        Console.Write("Digite sua senha: ");
+        senha = Console.ReadLine();
+
+        if (senha.Length < tamanho)
         {
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Login bem-sucedido!");
-            Console.WriteLine("Bem vindo Victor Mori Santos");
-            Console.WriteLine("-----------------------");
-        }
-        else if (nome != nomeUsuario && senha == senhaUsuario)
-        {
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Nome do usuário incorreto!");
-            Console.WriteLine("-----------------------");
-        }
-        else if (nome == nomeUsuario && senha != senhaUsuario)
-        {
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Senha do usuário incorreta!");
-            Console.WriteLine("-----------------------");
-        }
-        else
-        {
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Nome e Senha do usuário incorreta!");
-            Console.WriteLine("-----------------------");
+            Console.WriteLine($"A senha deve conter no minímo {tamanho} caracteres");
+            while (senha.Length < tamanho)
+            {
+                Console.Write("Digite sua senha novamente: ");
+                senha = Console.ReadLine();
+            }
         }
 
-        Console.WriteLine("Digite sua idade: ");
-        int idade = Convert.ToInt32(Console.ReadLine()); // ou int idade = int.Parse(Console.ReadLine())
+        Console.WriteLine("Seu cadastro foi bem sucedido!\n");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+
+        Console.WriteLine("----------------LOGIN----------------");
+
+        Console.Write("Digite seu email: ");
+        string emailLogin = Console.ReadLine();
+
+        Console.Write("Digite sua senha: ");
+        string senhaLogin = Console.ReadLine();
+        if (emailLogin != email || senhaLogin != senha)
+        {
+            Console.WriteLine("Suas informações estão incorretas!");
+            while (emailLogin != email || senhaLogin != senha)
+            {
+                Console.WriteLine("----------------LOGIN----------------");
+                Console.Write("Digite seu email: ");
+                emailLogin = Console.ReadLine();
+
+                Console.Write("Digite sua senha: ");
+                senhaLogin = Console.ReadLine();
+            }
+        }
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine(".");
+        Console.WriteLine($"--------------Olá {nome}, Bem vindo ao nosso site!------------ ");
 
 
 
 
-        
-    
 
-        
 
-       
+
+
+
+
     }
 }
